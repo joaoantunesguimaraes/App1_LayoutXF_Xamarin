31d7e06a3bbaadec13e0a93298e5973275b6a6df
João Antunes Guimarães <joao.antunes.guimaraes@gmail.com>
2022-10-14 00:04:07 +0100

    Commit Inicial

---
+++ App1_LayoutXF/App1_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1_LayoutXF
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        // Método do evento Clicked do Botão 
+        private void GoPaginaStack(object sender, EventArgs args)
+        {
+            // Abrir Página
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+
+
+        // Método do evento Clicked do Botão 
+        private void GoPaginaGrid(object sender, EventArgs args)
+        {
+            // Abrir Página
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+
+        // Método do evento Clicked do Botão 
+        private void GoPaginaAbsolute(object sender, EventArgs args)
+        {
+            // Abrir Página
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+
+        // Método do evento Clicked do Botão 
+        private void GoPaginaRelative(object sender, EventArgs args)
+        {
+            // Abrir Página
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+
+        // Método do evento Clicked do Botão 
+        private void GoPaginaScrool(object sender, EventArgs args)
+        {
+            // Abrir Página
+            Navigation.PushAsync(new Layouts.Scrool.ScroolPage());
+        }
+
+
+
+
+
+    }
+}
